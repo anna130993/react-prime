b4c41a41d3165c1daa80d0a04cfe86a060ea740d
Anna Podsiadlo <annamagdalena.podsiadlo@gmail.com>
2021-02-05 12:53:44 +0100

    Add Container for SearchResult

--- src/components/SearchResult/SearchResult.js
+++ src/components/SearchResult/SearchResult.js
@@ -2,13 +2,16 @@ import React from 'react';
 import styles from './SearchResult.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
+import Container from '../Container/Container.js';
 
 const SearchResult = ({cards}) => (
-  <section className={styles.component}>
-    {(cards).map(cardData => (
-      <Card key={cardData.id} draggable={false} {...cardData} />
-    ))}
-  </section>
+  <Container>
+    <section className={styles.component}>
+      {(cards).map(cardData => (
+        <Card key={cardData.id} draggable={false} {...cardData} />
+      ))}
+    </section>
+  </Container>
 );
 SearchResult.propTypes = {
   cards: PropTypes.array,
